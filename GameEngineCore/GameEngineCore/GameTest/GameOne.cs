4f015789fd1fc46410ed0c19831ee688e79477a9
pronom <ayoub.nazeer@gmail.com>
2017-11-25 15:33:34 +0100

    Adding class diagram

--- GameEngineCore/GameEngineCore/GameTest/GameOne.cs
+++ GameEngineCore/GameEngineCore/GameTest/GameOne.cs
@@ -13,12 +13,13 @@ namespace GameEngineCore.GameTest
     public class GameOne : AbstractGame
     {
         public Rectangle rect1 { get; private set; }
+        public Ellipse circle { get; private set; }
         private static int X;
         private static int Y;
 
         public override void Draw()
         {
-            
+
             rect1.Margin = new System.Windows.Thickness(X, Y, 0, 0);
         }
 
@@ -30,20 +31,25 @@ namespace GameEngineCore.GameTest
         public override void Init()
         {
             rect1 = new Rectangle();
+            
             rect1.Width = 100;
             rect1.Height = 100;
             rect1.Fill = Brushes.Black;
             Y = 0;
             X = 0;
 
-            
+            circle = new Ellipse();
+            circle.Width = 100;
+            circle.Height = 100;
+            circle.Fill = Brushes.Azure;
+           
             rect1.Margin = new System.Windows.Thickness(X, Y, 0, 0);
         }
 
         public override void Update()
         {
             X++;
-            Y++;
+            //Y++;
         }
     }
 }
